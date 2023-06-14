4122e4dc54476afe8de8a0614bb55f23de83eb06
thiothedev <thiothedev@proton.me>
2023-06-14 11:33:11 +0200

    BE: Adding and setting up Socket.io

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+import { Server } from 'socket.io';
+
+import { DEFAULT_SOCKET_PORT } from './utils/constants';
+
+// Ports
+export const socketPort =
+  Number(process.env.SOCKET_PORT) || DEFAULT_SOCKET_PORT;
+
+// Socket.io Server
+const io = new Server(socketPort);
+
+// Handling Connections
+io.on('connection', (socket) => {
+  console.log(`Connection: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnect: ${socket.id}`);
+  });
+});
