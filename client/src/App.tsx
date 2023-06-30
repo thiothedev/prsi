9713dd5a8e4eda08be542b5aa880318fc0347b3f
thiothedev <thiothedev@proton.me>
2023-06-30 07:12:45 +0200

    FE: Adding and setting up React Router DOM

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { HomePage } from './pages/HomePage';
+
+import { AppRoutes } from './utils/route';
+
 const App = (): JSX.Element => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Routes>
+    <Route path={AppRoutes.HOME} element={<HomePage />} />
+  </Routes>
 );
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export const HomePage = (): JSX.Element => (
+  <>
+    <h1>Home</h1>
+  </>
+);
